26e643c9c728521698f5597aa2d608c403e8d2f1
Ayush Gupta <gupta07ayush@gmail.com>
2023-01-03 22:37:32 +0530

    set window title

--- calculator.py
+++ calculator.py
@@ -6,3 +6,12 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
 import sys
+
+
+class Window(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        # setting title
+        self.setWindowTitle("Python Calculator")
